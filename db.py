a03103ad74128aec8f11b4678ac5455439448406
uttamkn <uttamkn15@gmail.com>
2024-10-05 21:13:30 +0530

    Refactor create_entry function to use image embeddings from vec_emb module

--- db.py
+++ db.py
@@ -1,9 +1,12 @@
 import datetime
+import os
 import sqlite3
 from typing import List
 
 import numpy as np
 import sqlite_vec
+
+from vec_emb import get_img_emb
 
 
 def create_connection(db_file: str) -> sqlite3.Connection:
@@ -29,9 +32,10 @@ def create_table(conn: sqlite3.Connection) -> None:
 
 
 # not sure about the type of vector_embedding using float[] for now
-def create_entry(conn: sqlite3.Connection, image_path: str, vector: np.ndarray) -> None:
+def create_entry(conn: sqlite3.Connection, image_path: str) -> None:
     cursor = conn.cursor()
     timestamp = datetime.datetime.now().isoformat()
+    vector = get_img_emb(image_path)
     cursor.execute(
         """
         INSERT INTO entries (image_path, vector_embedding, timestamp) 
@@ -53,3 +57,31 @@ def get_all_entries(conn: sqlite3.Connection) -> List[dict]:
             {"id": row[0], "image_path": row[1], "vector": vector, "timestamp": row[3]}
         )
     return results
+
+
+def add_images_from_directory(conn: sqlite3.Connection, directory: str) -> None:
+    for filename in os.listdir(directory):
+        if filename.endswith((".jpg", ".jpeg", ".png")):
+            image_path = os.path.join(directory, filename)
+            create_entry(conn, image_path)
+
+
+def print_entries(conn: sqlite3.Connection) -> None:
+    entries = get_all_entries(conn)
+    for entry in entries:
+        print(
+            f"ID: {entry['id']}, Image Path: {entry['image_path']}, Timestamp: {entry['timestamp']}, Vector: {entry['vector']}"
+        )
+
+
+if __name__ == "__main__":
+    db_file = "test.db"
+    conn = create_connection(db_file)
+    create_table(conn)
+
+    add_images_from_directory(conn, "smarn_screenshots")
+
+    print_entries(conn)
+
+    conn.close()
+
