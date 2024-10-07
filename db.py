596b196f33017ea1cdb35125e8a2ae2386c49225
Suchit G <suchitg04@gmail.com>
2024-10-07 11:56:56 +0530

    Add db funcs

--- db.py
+++ db.py
@@ -1,87 +1,78 @@
-import datetime
-import os
-import sqlite3
-from typing import List
-
-import numpy as np
 import sqlite_vec
+import sqlite3
 
+from sqlite_vec import serialize_float32
 from vec_emb import get_img_emb
+from textwrap import dedent
 
 
-def create_connection(db_file: str) -> sqlite3.Connection:
-    conn = sqlite3.connect(db_file)
-    conn.enable_load_extension(True)
-    sqlite_vec.load(conn)
-    return conn
+class Database:
+    def __init__(self, db_file):
+        self.db_file = db_file
+        self.conn = sqlite3.connect(self.db_file)
+        self.conn.enable_load_extension(True)
+        sqlite_vec.load(self.conn)
+        self.conn.enable_load_extension(False)
 
-
-# TODO: Add window title and application name
-def create_table(conn: sqlite3.Connection) -> None:
-    create_table_sql = """
-    CREATE TABLE IF NOT EXISTS entries (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        image_path TEXT NOT NULL,
-        vector_embedding float[] NOT NULL,
-        timestamp TEXT NOT NULL
-    );
-    """
-    cursor = conn.cursor()
-    cursor.execute(create_table_sql)
-    conn.commit()
-
-
-# not sure about the type of vector_embedding using float[] for now
-def create_entry(conn: sqlite3.Connection, image_path: str) -> None:
-    cursor = conn.cursor()
-    timestamp = datetime.datetime.now().isoformat()
-    vector = get_img_emb(image_path)
-    cursor.execute(
-        """
-        INSERT INTO entries (image_path, vector_embedding, timestamp) 
-        VALUES (?, ?, ?)
-    """,
-        (image_path, vector, timestamp),
-    )
-    conn.commit()
-
-
-def get_all_entries(conn: sqlite3.Connection) -> List[dict]:
-    cursor = conn.cursor()
-    cursor.execute("SELECT id, image_path, vector_embedding, timestamp FROM entries;")
-    rows = cursor.fetchall()
-    results = []
-    for row in rows:
-        vector = np.frombuffer(row[2], dtype=np.float32)
-        results.append(
-            {"id": row[0], "image_path": row[1], "vector": vector, "timestamp": row[3]}
-        )
-    return results
-
-
-def add_images_from_directory(conn: sqlite3.Connection, directory: str) -> None:
-    for filename in os.listdir(directory):
-        if filename.endswith((".jpg", ".jpeg", ".png")):
-            image_path = os.path.join(directory, filename)
-            create_entry(conn, image_path)
-
-
-def print_entries(conn: sqlite3.Connection) -> None:
-    entries = get_all_entries(conn)
-    for entry in entries:
+        version_info = self.conn.execute("SELECT sqlite_version(), vec_version()").fetchone()
         print(
-            f"ID: {entry['id']}, Image Path: {entry['image_path']}, Timestamp: {entry['timestamp']}, Vector: {entry['vector']}"
+            dedent(f"""
+                Database initialized.
+                vec_version(): {version_info[0]}
+                sqlite_version(): {version_info[1]}
+            """)
         )
 
+    # TODO: Add window title and application name
+    def create_tables(self) -> None:
+        self.conn.execute(
+            """
+                CREATE TABLE IF NOT EXISTS img_info (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    image_path TEXT NOT NULL,
+                    timestamp TIMESTAMP NOT NULL
+                );
+            """
+        )
+        self.conn.execute(
+            """
+                CREATE VIRTUAL TABLE IF NOT EXISTS vec_idx USING vec0  (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    embedding FLOAT[768]
+                );
+            """
+        )
+        self.conn.commit()
 
-if __name__ == "__main__":
-    db_file = "test.db"
-    conn = create_connection(db_file)
-    create_table(conn)
 
-    add_images_from_directory(conn, "smarn_screenshots")
+    def insert_entry(self, image_path: str) -> None:
+        embedding = get_img_emb(image_path)
+        self.conn.execute(
+        """
+                INSERT INTO img_info (image_path, timestamp) 
+                VALUES (?, datetime())
+            """,
+            (image_path, ),
+        )
+        self.conn.execute(
+            """
+                INSERT INTO vec_idx (embedding)
+                VALUES (?)
+            """,
+            (serialize_float32(embedding), ),
+        )
+        self.conn.commit()
 
-    print_entries(conn)
-
-    conn.close()
+    def get_last_entry(self) -> tuple | None:
+        last_entry = self.conn.execute(
+            """
+                SELECT * FROM img_info
+                ORDER BY timestamp DESC 
+                LIMIT 1;
+            """
+        ).fetchone()
+        if last_entry:
+            return last_entry
+        else:
+            return None
 
--- vec_emb.py
+++ vec_emb.py
@@ -1,32 +1,15 @@
-import os
-from typing import Optional
-
 import numpy as np
+import os
+
 from transformers import AutoModel
 
 model = AutoModel.from_pretrained("jinaai/jina-clip-v1", trust_remote_code=True)
 
 
-def get_img_emb(path: str) -> Optional[np.ndarray]:
+def get_img_emb(path: str) -> np.ndarray:
     if not os.path.exists(path):
-        print("Image path does not exist!")
-        return None
-
+        raise ValueError("Image path does not exist!")
     return model.encode_image(path)
-
 
 def get_text_emb(text: str) -> np.ndarray:
     return model.encode_text(text)
-
-
-def get_dot_product(img_emb: np.ndarray, text_emb: np.ndarray) -> float:
-    return float(text_emb @ img_emb.T)
-
-
-if __name__ == "__main__":
-    img_emb = get_img_emb("smarn_screenshots/keeb.jpg")
-    text_emb = get_text_emb("keyboard")
-
-    if img_emb:
-        print(get_dot_product(img_emb, text_emb))
-
