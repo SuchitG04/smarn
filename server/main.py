9af56a2fc5274d8ae716dfdbfe3264c30aab0ef6
Mohit <themohitnair@protonmail.com>
2024-10-09 18:02:17 +0530

    Added database insertion operations to main.py

--- server/main.py
+++ server/main.py
@@ -9,18 +9,21 @@ rate: float = 2.0
 
 def smarn():
     global rate
+    Ddb = Database()
     while True:
         current_screenshot_path: str = capture()
         time.sleep(rate * 60)
 
-        # TODO: insert images into database
         if curr_emb := compare_with_prev_img(current_screenshot_path):
             if rate >= 5:
                 rate += 0.5
         else:
             if rate <= 2:
                 rate -= 0.5
-
+        
+        # inserting images into database
+        active_application_name = get_active_application_name()
+        Database.insert_entry(current_screenshot_path, active_application_name)
 
 if __name__ == "__main__":
     smarn()
