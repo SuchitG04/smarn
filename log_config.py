698fcd94f8412a8f287c1c4802dfcf1dc40d06c9
Mohit <themohitnair@protonmail.com>
2024-10-17 22:40:59 +0530

    Removed unnecessary notebooks

---
+++ log_config.py
@@ -0,0 +1,18 @@
+import logging
+
+def setup_logging():
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+
+    file_handler = logging.FileHandler('app.log')
+    file_handler.setLevel(logging.DEBUG) 
+    file_formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    file_handler.setFormatter(file_formatter)
+
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.INFO) 
+    console_formatter = logging.Formatter('%(levelname)s - %(message)s')
+    console_handler.setFormatter(console_formatter)
+
+    logger.addHandler(file_handler)
+    logger.addHandler(console_handler)
