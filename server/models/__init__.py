70682728485b8ce635329a26e759d4ae098727a0
uttamkn <uttamkn15@gmail.com>
2024-10-17 18:37:22 +0530

    Add VRAM check to fallback to CPU model on low-memory GPUs.

--- server/models/__init__.py
+++ server/models/__init__.py
@@ -1,26 +1,41 @@
-# Loads the appropriate model and processor based on the system's hardware.
-# - If a GPU is available:
-#     - Loads the colpali model with 8-bit quantization.
-#     - Retrieves the ColPali processor and assigns it
-#         to the `processor` variable, and the model is assigned to `model`.
-#     - The `device` variable is updated to "gpu".
+"""
+Loads the appropriate model and processor based on the system's hardware.
 
-# - If no GPU is available:
-#     - Loads the AutoModel from "jinaai/jina-clip-v1" and assigns it to `model`.
-#     - The `device` variable is updated to "cpu".
-#     - The `processor` variable is not assigned as it is not required for the CPU model.
+ - If a GPU is available:
+     - Loads the colpali model with 8-bit quantization.
+     - Retrieves the ColPali processor and assigns it
+         to the `processor` variable, and the model is assigned to `model`.
+     - The `device` variable is updated to "gpu".
 
-import torch.cuda as cuda
+ - If no GPU is available:
+     - Loads the AutoModel from "jinaai/jina-clip-v1" and assigns it to `model`.
+     - The `device` variable is updated to "cpu".
+     - The `processor` variable is not assigned as it is not required for the CPU model.
+"""
+
+import torch
+
 from .colpali import load_gpu_model
 from .jina_clip import load_cpu_model
 
 model = None
 processor = None
 device = None
+REQUIRED_MEMORY = 7.0
+
+
+def get_gpu_vram():
+    """
+    Returns the total VRAM available on the GPU in GB.
+    """
+    return torch.cuda.get_device_properties(torch.device("cuda:0")).total_memory / (
+        1024**3
+    )
+
 
 if model is None:
     try:
-        if cuda.is_available():
+        if torch.cuda.is_available() and get_gpu_vram() > REQUIRED_MEMORY:
             model, processor = load_gpu_model()
             device = "gpu"
         else:
