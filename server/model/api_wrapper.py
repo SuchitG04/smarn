848d167309c17883e483a53e0b90cb2f4a82c52f
Suchit G <suchitg04@gmail.com>
2024-12-09 13:48:16 +0530

    fix: change return types in api wrapper

--- server/model/api_wrapper.py
+++ server/model/api_wrapper.py
@@ -1,24 +1,22 @@
+import numpy as np
 import requests
 
 BASE_URL = "http://localhost:6942"
 
 
-# TODO: convert the response to numpy array
-
-
-async def get_image_embs(image_path: str) -> list[float]:
+async def get_image_embs(image_path: str) -> np.ndarray:
     try:
         res = requests.get(f"{BASE_URL}/imgemb/?image_path={image_path}")
     except:
         raise Exception("Unable to response from /imgemb endpoint")
 
-    return res.json()
+    return np.array(res.json())
 
 
-async def get_text_embs(query: str) -> list[float]:
+async def get_text_embs(query: str) -> np.ndarray:
     try:
         res = requests.get(f"{BASE_URL}/textemb/?query={query}")
     except:
         raise Exception("Unable to response from /textemb endpoint")
 
-    return res.json()
+    return np.array(res.json())
