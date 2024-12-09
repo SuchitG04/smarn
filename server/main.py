2deb72da0a95e71231354184c5062dcada58eff8
uttamkn <uttamkn15@gmail.com>
2024-12-09 15:12:33 +0530

    fix: update main.py to use asyncio for service execution and adjust import in model

--- server/main.py
+++ server/main.py
@@ -1,3 +1,5 @@
+import asyncio
+
 import config.log_config  # setup logging
 
 # import uvicorn
@@ -5,10 +7,7 @@ from model.jina_clip import load_cpu_model
 from screenshot import service
 
 if __name__ == "__main__":
-    state = load_cpu_model()
-
     # TODO: Run both of these as two different processes using multiprocessor
     # uvicorn.run("api.main:app", host="localhost", port=8000, reload=True)
 
-    # TODO: Await the service function in main
-    service()
+    asyncio.run(service())
